35b27f85629f79ccca841170152199fd0744e7a8
tim-fabian <fabian@service-soft.de>
2023-05-17 23:35:19 +0200

    Fixed soft delete files not being included in index

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,5 @@
 export * from './change-set-entity.model';
+export * from './change-set-soft-delete-entity.model';
 export * from './change-set-type.enum';
 export * from './change-set.model';
 export * from './change.model';
